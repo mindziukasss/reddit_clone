bdc4dc0b9a7fcbcc04af951337db4bfc98abb5d4
mindziukasss <mindziukasss@gmail.com>
2017-01-27 13:22:49 +0200

    Added signup form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
 def signup(request):
-	return render(request, 'accounts/signup.html')
+	if request.method == 'POST':
+		 User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+		 return render(request, 'accounts/signup.html')
+	else:
+
+			return render(request, 'accounts/signup.html')
