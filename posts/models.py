6d9eca4a716e4d9244b4c220e2f973b4762be357
mindziukasss <mindziukasss@gmail.com>
2017-01-27 15:58:10 +0200

    Implemented complete post creatio

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
 	title = models.CharField(max_length=200)
 	url = models.TextField()
 	pub_date = models.DateTimeField()
