a6f1bdb50d056d74ff646b3b76c3e4edd89d59d8
mindziukasss <mindziukasss@gmail.com>
2017-01-27 16:16:10 +0200

    Implemented post listing on home page

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from . import models
+from .models import Post
 # Create your views here.
 @login_required
 def create(request):
 	if request.method == 'POST':
 			if request.POST['title'] and request.POST['url']:
-					post = models.Post()
+					post = Post()
 					post.title = request.POST['title']
 					post.url = request.POST['url']
 					post.pub_date = timezone.datetime.now()
 					post.author = request.user
 					post.save()
-					return render(request, 'posts/home.html')
-
+					return redirect('home')
 			else:
 					return render(request, 'posts/create.html',{'error':'ERROR: You must include a title and url to create Post'})
 
@@ -23,4 +22,5 @@ def create(request):
 			return render(request, 'posts/create.html')
 
 def home(request):
-			return render(request, 'posts/home.html')
+			posts = Post.objects.order_by('votes_total')
+			return render(request, 'posts/home.html', {'posts':posts})
