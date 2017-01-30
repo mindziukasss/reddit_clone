3d1be85f79dd5c2c9bb403706246153236c11d1d
mindziukasss <mindziukasss@gmail.com>
2017-01-30 16:05:44 +0200

    Styled home page

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
 	pub_date = models.DateTimeField()
 	author = models.ForeignKey(User)
 	votes_total = models.IntegerField(default=1)
+
+	def pub_date_pretty(self):
+			return self.pub_date.strftime('%b %e %Y')
